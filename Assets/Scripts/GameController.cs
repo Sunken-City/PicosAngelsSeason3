6dd419ad8a4daaa33e6c2307a8b76bc39a4a95dd
Anthony Cloudy <acloudy@mail.smu.edu>
2017-04-21 23:35:09 -0500

    Added the Game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    void Awake()
+    {
+        // hostile singleton
+        if (instance)
+        {
+            Debug.Log("Destroying irrelevant GameController instance");
+            Destroy(instance.gameObject);
+        }
+
+        instance = this;
+    }
+    
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+}
