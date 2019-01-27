d0276f976dc3674dd9dc81bf5537d994fd07029f
Anthony Cloudy <acloudy@mail.smu.edu>
2019-01-26 17:12:51 -0800

    Made it more logical and sensical

--- Assets/Editor/ChatInspector.cs
+++ Assets/Editor/ChatInspector.cs
@@ -25,41 +25,41 @@ public class ChatEditor : Editor
 
             chat.chatCommands[i].leftCharacter = (GameObject)EditorGUILayout.ObjectField("Left Character:", chat.chatCommands[i].leftCharacter, typeof(GameObject), false);
             chat.chatCommands[i].leftExpression = (Character.Animations)EditorGUILayout.EnumPopup("Expression:", chat.chatCommands[i].leftExpression);
-            chat.chatCommands[i].rightCharacter = (GameObject)EditorGUILayout.ObjectField("Right Character:", chat.chatCommands[i].rightCharacter, typeof(GameObject), false);
-            chat.chatCommands[i].rightExpression = (Character.Animations)EditorGUILayout.EnumPopup("Expression:", chat.chatCommands[i].rightExpression);
             if (chat.chatCommands[i].leftCharacter)
             {
                 Texture2D myTexture = AssetPreview.GetAssetPreview(chat.chatCommands[i].leftCharacter.GetComponent<Character>().emotions[(int)chat.chatCommands[i].leftExpression]);
                 GUILayout.Label(myTexture);
             }
+
+            chat.chatCommands[i].rightCharacter = (GameObject)EditorGUILayout.ObjectField("Right Character:", chat.chatCommands[i].rightCharacter, typeof(GameObject), false);
+            chat.chatCommands[i].rightExpression = (Character.Animations)EditorGUILayout.EnumPopup("Expression:", chat.chatCommands[i].rightExpression);
             if (chat.chatCommands[i].rightCharacter)
             {
                 Texture2D myTexture = AssetPreview.GetAssetPreview(chat.chatCommands[i].rightCharacter.GetComponent<Character>().emotions[(int)chat.chatCommands[i].rightExpression]);
                 GUILayout.Label(myTexture);
             }
+            
             chat.chatCommands[i].speakerName = EditorGUILayout.TextField("Speaker Name:", chat.chatCommands[i].speakerName);
             GUILayout.Label("Dialogue Text:");
             chat.chatCommands[i].dialogueText = EditorGUILayout.TextArea(chat.chatCommands[i].dialogueText, GUILayout.MaxHeight(50));
 
             Seperator();
 
-            if (GUILayout.Button("Insert New Chat Command Here"))
+            if (GUILayout.Button("Insert New", GUILayout.Width(100)))
                 chat.chatCommands.Insert(i + 1, new Chat.Command());
 
             Seperator();
         }
 
         if (GUILayout.Button("Add New Chat Command"))
+        {
             chat.chatCommands.Add(new Chat.Command());
-
-        if (GUILayout.Button("Remove Last Chat Command"))
-            chat.chatCommands.RemoveAt(chat.chatCommands.Count - 1);
-
+        }
         EditorUtility.SetDirty(chat);
     }
 
     void Seperator()
     {
-        GUILayout.Label("-----------------------------------");
+        GUILayout.Label("---------------------------------------------------------------------");
     }
 }
